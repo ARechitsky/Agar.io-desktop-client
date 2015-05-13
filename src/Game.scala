5d0e989b313a5c30843f87ec9b20250850bbe65b
Andrey Rechitsky <arechitsky@gmail.com>
2015-05-13 20:27:49 +0700

    Move UI to Game class

--- src/Game.scala
+++ src/Game.scala
@@ -1,20 +1,42 @@
+import javax.swing.Timer
+import scala.swing._
 
-class Game(address: String) {
+class Game(address: String) extends MainFrame {
   val state = new GameState
   val networkAdapter = new NetworkAdapter(address, state)
+  val canvas = new GameFieldPanel(state)
+  val fpsLabel = new Label()
+
+  title = "Agar.io desktop client"
+
+  listenTo(canvas)
+  reactions += {
+    case NeedMoveEvent(_, x, y) =>
+      networkAdapter.sendMoveTo(x, y)
+    case NeedSplit(_) =>
+      networkAdapter sendSplit()
+    case NeedSpit(_) =>
+      networkAdapter sendSpit()
+
+  }
+  contents = new BoxPanel(Orientation.Vertical) {
+    contents += canvas
+    contents += fpsLabel
+  }
+  val timer = new Timer(10, Swing.ActionListener(e => {
+    canvas update()
+  }))
+  timer.start()
+  val secondTimer = new Timer(1000, Swing.ActionListener(e => {
+    state fixFpsAndDps()
+    fpsLabel.text = "FPS: %d, Data updates count: %d".format(state.fps, state.dps)
+  }))
+  secondTimer.start()
+  size = new Dimension(1000, 600)
+  canvas.requestFocus()
+
   networkAdapter connect()
 
   networkAdapter sendNick "asf"
 
-  def moveTo(x: Double, y: Double) {
-    networkAdapter.sendMoveTo(x, y)
-  }
-
-  def split() {
-    networkAdapter sendSplit()
-  }
-
-  def spit() {
-    networkAdapter sendSpit()
-  }
 }
--- src/Main.scala
+++ src/Main.scala
@@ -1,39 +1,5 @@
-import javax.swing.Timer
 import scala.swing._
 
 object Main extends SimpleSwingApplication {
-  //val g = new Game("echo.websocket.org")
-
-  override def top = new MainFrame {
-    title = "Agar.io desktop client"
-    val g = new Game("213.219.38.70:443")
-
-    val canvas = new GameFieldPanel(g.state)
-    val fpsLabel = new Label()
-    listenTo(canvas)
-    reactions += {
-      case NeedMoveEvent(component, x, y) if component == canvas =>
-        g.moveTo(x, y)
-      case NeedSplit(component) if component == canvas =>
-        g split()
-      case NeedSpit(component) if component == canvas =>
-        g spit()
-
-    }
-    contents = new BoxPanel(Orientation.Vertical) {
-      contents += canvas
-      contents += fpsLabel
-    }
-    val timer = new Timer(10, Swing.ActionListener(e => {
-      canvas update()
-    }))
-    timer.start()
-    val secondTimer = new Timer(1000, Swing.ActionListener(e => {
-      g.state fixFpsAndDps()
-      fpsLabel.text = "FPS: %d, Data updates count: %d".format(g.state.fps, g.state.dps)
-    }))
-    secondTimer.start()
-    size = new Dimension(1000, 600)
-    canvas.requestFocus()
-  }
+  override def top = new Game("213.168.249.134:443")
 }
